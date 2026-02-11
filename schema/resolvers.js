b7a274f0d716205c9d434a3934ddd5d8107f79af
REDDY PRO <youremail@example.com>
2026-02-11 16:16:09 -0600

    Storing

---
+++ schema/resolvers.js
@@ -0,0 +1,26 @@
+const { Query } = require("mongoose")
+const User = require("../models/User.js")
+
+const resolvers = {
+  Query: {
+    async getUsers() {
+      return await User.find()
+    },
+  },
+  Mutation: {
+    async createUser(_, { userInput: { id, name, age, gender } }) {
+      const createUser = new User({
+        id: id,
+        name: name,
+        age: age,
+        gender: gender,
+      })
+
+      const res = await createUser.save()
+
+      return { id: res.id, ...res._doc }
+    },
+  },
+}
+
+module.exports = { resolvers }
